7b5e7665b9a0b464855f364162de1558cb4f792b
ahortigu <anaisabel.hortiguela@gmail.com>
2022-04-12 19:58:04 +0100

    Project created

---
+++ app/src/main/java/com/aihg/gestionatumenu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aihg.gestionatumenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
