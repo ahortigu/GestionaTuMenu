94c223f716ed57c4215f07fafd9fd65db24f2f4b
ahortigu <anaisabel.hortiguela@gmail.com>
2022-04-18 18:34:53 +0100

    Implemented BottomNav y Toolbar with Navigation. Home fully functional.

--- app/src/main/java/com/aihg/gestionatumenu/HomeFragment.java
+++ app/src/main/java/com/aihg/gestionatumenu/HomeFragment.java
@@ -2,63 +2,89 @@ package com.aihg.gestionatumenu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    Button bIngredientes, bListaCompra, bDespenda, bRecetas, bPlanificador, bMenu;
     public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Botones
+        bIngredientes = view.findViewById(R.id.bIngredientes);
+        bListaCompra = view.findViewById(R.id.bListaCompra);
+        bDespenda = view.findViewById(R.id.bDespensa);
+        bRecetas = view.findViewById(R.id.bRecetas);
+        bPlanificador = view.findViewById(R.id.bPlanificador);
+        bMenu = view.findViewById(R.id.bMenu);
+
+        // Added listener
+        bIngredientes.setOnClickListener(this);
+        bListaCompra.setOnClickListener(this);
+        bDespenda.setOnClickListener(this);
+        bRecetas.setOnClickListener(this);
+        bPlanificador.setOnClickListener(this);
+        bMenu.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        NavDirections action;
+        switch (view.getId()) {
+            case R.id.bIngredientes:
+                action = HomeFragmentDirections.actionHomeFragmentToIngredientesFragment();
+                Navigation.findNavController(view).navigate(action);
+                break;
+            case R.id.bListaCompra:
+                action = HomeFragmentDirections.actionHomeFragmentToListaCompraFragment();
+                Navigation.findNavController(view).navigate(action);
+                break;
+            case R.id.bDespensa:
+                action = HomeFragmentDirections.actionHomeFragmentToDespensaFragment();
+                Navigation.findNavController(view).navigate(action);
+                break;
+            case R.id.bRecetas:
+                action = HomeFragmentDirections.actionHomeFragmentToRecetasFragment();
+                Navigation.findNavController(view).navigate(action);
+                break;
+            case R.id.bPlanificador:
+                action = HomeFragmentDirections.actionHomeFragmentToPlanificadorFragment();
+                Navigation.findNavController(view).navigate(action);
+                break;
+            case R.id.bMenu:
+                 action = HomeFragmentDirections.actionHomeFragmentToMenuFragment();
+                Navigation.findNavController(view).navigate(action);
+                break;
+        }
     }
 }
--- app/src/main/java/com/aihg/gestionatumenu/RecetasFragment.java
+++ app/src/main/java/com/aihg/gestionatumenu/RecetasFragment.java
@@ -8,9 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+public class RecetasFragment extends Fragment {
 
-public class IngredientesFragment extends Fragment {
-    public IngredientesFragment() {
+    public RecetasFragment() {
     }
 
     @Override
@@ -20,6 +20,6 @@ public class IngredientesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ingredientes, container, false);
+        return inflater.inflate(R.layout.fragment_recetas, container, false);
     }
 }
