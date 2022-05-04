6ecf157828abd69be894e4b7932ed280370b806f
ahortigu <anaisabel.hortiguela@gmail.com>
2022-05-04 11:24:51 +0100

    Added ListaCompra view

--- app/src/main/java/com/aihg/gestionatumenu/ListaCompraFragment.java
+++ app/src/main/java/com/aihg/gestionatumenu/ListaCompraFragment.java
@@ -20,6 +20,6 @@ public class ListaCompraFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lista_compra, container, false);
+        return inflater.inflate(R.layout.listacompra__fragment, container, false);
     }
 }
