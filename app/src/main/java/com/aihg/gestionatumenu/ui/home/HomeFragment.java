860bff50632eba16b0a29801a5df9993ca21a3ed
ahortigu <anaisabel.hortiguela@gmail.com>
2022-05-13 16:24:07 +0100

    Improved Home xml

--- app/src/main/java/com/aihg/gestionatumenu/ui/home/HomeFragment.java
+++ app/src/main/java/com/aihg/gestionatumenu/ui/home/HomeFragment.java
@@ -39,12 +39,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         super.onViewCreated(view, savedInstanceState);
 
         // Botones
-        bIngredientes = view.findViewById(R.id.bIngredientes);
-        bListaCompra = view.findViewById(R.id.bListaCompra);
-        bDespenda = view.findViewById(R.id.bDespensa);
-        bRecetas = view.findViewById(R.id.bRecetas);
-        bPlanificador = view.findViewById(R.id.bPlanificador);
-        bMenu = view.findViewById(R.id.bMenu);
+        bIngredientes = view.findViewById(R.id.b_Ingredientes);
+        bListaCompra = view.findViewById(R.id.b_lista_compra);
+        bDespenda = view.findViewById(R.id.b_despensa);
+        bRecetas = view.findViewById(R.id.b_recetas);
+        bPlanificador = view.findViewById(R.id.b_planificador);
+        bMenu = view.findViewById(R.id.b_menu);
 
         // Added listener
         bIngredientes.setOnClickListener(this);
@@ -67,27 +67,27 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         NavDirections action;
         switch (view.getId()) {
-            case R.id.bIngredientes:
+            case R.id.b_Ingredientes:
                 action = HomeFragmentDirections.actionHomeFragmentToIngredientesFragment();
                 Navigation.findNavController(view).navigate(action);
                 break;
-            case R.id.bListaCompra:
+            case R.id.b_lista_compra:
                 action = HomeFragmentDirections.actionHomeFragmentToListaCompraFragment();
                 Navigation.findNavController(view).navigate(action);
                 break;
-            case R.id.bDespensa:
+            case R.id.b_despensa:
                 action = HomeFragmentDirections.actionHomeFragmentToDespensaFragment();
                 Navigation.findNavController(view).navigate(action);
                 break;
-            case R.id.bRecetas:
+            case R.id.b_recetas:
                 action = HomeFragmentDirections.actionHomeFragmentToRecetasFragment();
                 Navigation.findNavController(view).navigate(action);
                 break;
-            case R.id.bPlanificador:
+            case R.id.b_planificador:
                 action = HomeFragmentDirections.actionHomeFragmentToPlanificadorFragment();
                 Navigation.findNavController(view).navigate(action);
                 break;
-            case R.id.bMenu:
+            case R.id.b_menu:
                  action = HomeFragmentDirections.actionHomeFragmentToMenuFragment();
                 Navigation.findNavController(view).navigate(action);
                 break;
