b0d91b92420a9cd618eb489c0b28065ed77aca87
ahortigu <anaisabel.hortiguela@gmail.com>
2022-05-17 20:06:22 +0100

    Moved Toast messages to constants. Added delete functionality on ListaCompra

--- app/src/main/java/com/aihg/gestionatumenu/ui/shared/util/GestionaTuMenuConstants.java
+++ app/src/main/java/com/aihg/gestionatumenu/ui/shared/util/GestionaTuMenuConstants.java
@@ -5,4 +5,15 @@ public class GestionaTuMenuConstants {
     public static final String NO_DESPENSA = "No hay Ingredientes";
     public static final String NO_LISTA_COMPRA = "No hay Ingredientes";
     public static final String NO_RECETA = "No hay Recetas";
+
+    public static final String TOAST_BORRAR_LISTA_COMPRA = "¡Item borrado de la lista!";
+    public static final String TOAST_NO_EXISTE_RECETA = "La receta no existe";
+    public static final String TOAST_NO_EXISTE_INGREDIENTE_LISTA = "El ingrediente no existe en la lista";
+    public static final String TOAST_NO_EXISTE_INGREDIENTE = "El ingrediente no existe";
+    public static final String TOAST_CREAR_INGREDIENTE = "Ingrediente creado con éxito";
+    public static final String TOAST_CREAR_INGREDIENTE_YA_EXISTE = "¡El ingrediente ya existe!";
+    public static final String TOAST_PLANIFICADOR_LIMPIO = "¡Planificador limpio!";
+    public static final String TOAST_PLANIFICADOR_APLICADO = "¡Planificador aplicado!";
+
+    public static final String TOAST_CAMPO_VACIO = "Por favor, no deje ningún campo vacío";
 }
