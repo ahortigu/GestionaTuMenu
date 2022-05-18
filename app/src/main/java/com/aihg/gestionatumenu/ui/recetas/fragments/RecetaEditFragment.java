3feaabf502fafb446640e5a41ac7ab0cf9fc1858
ahortigu <anaisabel.hortiguela@gmail.com>
2022-05-18 22:04:42 +0100

    Loaded on Edit Recetas some details.

--- app/src/main/java/com/aihg/gestionatumenu/ui/recetas/fragments/RecetaEditFragment.java
+++ app/src/main/java/com/aihg/gestionatumenu/ui/recetas/fragments/RecetaEditFragment.java
@@ -2,28 +2,100 @@ package com.aihg.gestionatumenu.ui.recetas.fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aihg.gestionatumenu.R;
+import com.aihg.gestionatumenu.db.entities.Receta;
+import com.aihg.gestionatumenu.ui.recetas.viewmodel.RecetasViewModel;
 
 
 public class RecetaEditFragment extends Fragment {
+    private View view;
+    private RecyclerView recyclerView;
+
+    private RecetasViewModel viewModel;
+
+    private Receta receta;
+
+    private EditText et_rce_nombre_receta;
+
+    private boolean isIngredienteExpandido;
+    private ConstraintLayout l_rd_expandable_ingredientes_parent;
+    private ConstraintLayout l_rd_expandable_ingredientes;
+    private ImageView iv_rd_arrow_ingredientes;
+    private ImageView iv_rce_plus;
+
+    private boolean isInstruccionesExpandido;
+    private ConstraintLayout l_rce_expandable_instrucciones_parent;
+    private ConstraintLayout l_rce_expandable_instrucciones;
+    private ImageView iv_rce_instrucciones;
+    private EditText et_rce_instrucciones;
+
+    private TextView txt_rd_categoria;
+
     public RecetaEditFragment() {
+        this.isIngredienteExpandido = true;
+        this.isInstruccionesExpandido = true;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        receta = RecetaEditFragmentArgs.fromBundle(getArguments()).getAModificar();
+        viewModel = new ViewModelProvider(this).get(RecetasViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.recetas__edit_create_fragment, container, false);
+        this.view = inflater.inflate(R.layout.recetas__edit_create_fragment, container, false);
+
+        this.recyclerView = (RecyclerView) view.findViewById(R.id.rv_rce_ingredientes);
+        this.recyclerView.setHasFixedSize(false);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        loadNombreReceta();
+        loadInstrucciones();
+
+        return view;
+    }
+
+    private void loadNombreReceta() {
+        this.et_rce_nombre_receta = view.findViewById(R.id.et_rce_nombre_receta);
+        et_rce_nombre_receta.setText(receta.getNombre());
+    }
+
+    private void loadInstrucciones() {
+        this.l_rce_expandable_instrucciones_parent = view.findViewById(R.id.l_rce_instrucciones);
+        this.l_rce_expandable_instrucciones = view.findViewById(R.id.l_rce_expandable_instrucciones);
+        this.iv_rce_instrucciones = view.findViewById(R.id.iv_rce_instrucciones);
+        this.et_rce_instrucciones = view.findViewById(R.id.et_rce_instrucciones);
+
+        this.et_rce_instrucciones.setText(receta.getInstrucciones());
+        this.l_rce_expandable_instrucciones.setVisibility(View.VISIBLE);
+        this.iv_rce_instrucciones.setImageResource(R.drawable.ic_arrow_up);
+
+        this.l_rce_expandable_instrucciones_parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isInstruccionesExpandido = !isInstruccionesExpandido;
+                l_rce_expandable_instrucciones.setVisibility(
+                        isInstruccionesExpandido ? View.VISIBLE : View.GONE
+                );
+                iv_rce_instrucciones.setImageResource(
+                        isInstruccionesExpandido ? R.drawable.ic_arrow_up : R.drawable.ic_arrow_down
+                );
+            }
+        });
     }
 }
