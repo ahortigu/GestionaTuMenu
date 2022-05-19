b890c440bb04f3e5514d35a76b20748f6ecc47df
ahortigu <anaisabel.hortiguela@gmail.com>
2022-05-19 16:09:19 +0100

    Improved spinners layout

--- app/src/main/java/com/aihg/gestionatumenu/ui/util/AbstractSpinner.java
+++ app/src/main/java/com/aihg/gestionatumenu/ui/util/AbstractSpinner.java
@@ -40,7 +40,6 @@ public abstract class AbstractSpinner<T> extends ArrayAdapter<T>  {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         TextView textoSeleccionado = (TextView) super.getView(position, convertView, parent);
-        textoSeleccionado.setTextColor(R.color.gris_oscuro);
         textoSeleccionado.setText(getNombreItem(getItem(position)));
         return textoSeleccionado;
     }
@@ -49,7 +48,6 @@ public abstract class AbstractSpinner<T> extends ArrayAdapter<T>  {
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         TextView textoDropdown = (TextView) super.getDropDownView(position, convertView, parent);
-        textoDropdown.setTextColor(R.color.gris_oscuro);
         textoDropdown.setText(getNombreItem(getItem(position)));
         return textoDropdown;
     }
